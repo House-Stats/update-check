73e067d1e9263632d38b249fc105c088c8711f05
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-02-09 11:50:27 +0000

    Added sentry sdk

--- checker/__init__.py
+++ checker/__init__.py
@@ -1,5 +1,10 @@
 from check_update import checkForUpdate
+import sentry_sdk
 
 if __name__ == "__main__":
+    sentry_sdk.init(
+    dsn="https://aadfc763299c4541b15f33e414c395d4@o4504585220980736.ingest.sentry.io/4504649935945728",
+    trace=1.0
+    )
     x = checkForUpdate()
     x.run()
