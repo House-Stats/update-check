d646446093beb2a0de7984f63d8f3b32858fa63b
Morgan Thomas <morganrhysthoams14@gmail.com>
2023-03-04 17:22:30 +0000

    Changed sentry DSN

--- checker/__init__.py
+++ checker/__init__.py
@@ -3,7 +3,7 @@ import sentry_sdk
 
 if __name__ == "__main__":
     sentry_sdk.init(
-        dsn="https://aadfc763299c4541b15f33e414c395d4@o4504585220980736.ingest.sentry.io/4504649935945728",
+        dsn="https://215c17a6719441f591985436380839cf@o4504585220980736.ingest.sentry.io/4504781485309952",
         traces_sample_rate=1.0
     )
     x = checkForUpdate()
